d1033b88fc8b955f3551fd7adcbe6a49cc3b814a
tw1ster4k <macbookpro@MacBook-Pro-Macbook.local>
2023-10-20 15:26:46 +0200

    release1.2.0

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Head from "next/head"
 import { useState } from "react"
 import styles from "../styles/index.module.css"
+
 const index = () => {
   const [name, setName] = useState('')
   const [tel, setTel] = useState('')
+  
 
   const submitFunction = (event) => {
     event.preventDefault()
@@ -22,14 +24,14 @@ const index = () => {
         <form className={styles.form} >
           <div className={styles.itemForm} >
           <p>Имя</p>
-              <input className={styles.input} required style={{paddingLeft:"7px"}} onChange={(event) => setName(event.target.value)} />
+              <input className={styles.input} style={{paddingLeft:"7px"}} required name="name"  onChange={(event) => setName(event.target.value)} />
           </div>
               <div className={styles.itemForm}>
                 <p>Номер телефона</p>
                 <i className={styles.numberStart} aria-hidden="true">+7</i>
-              <input className={styles.input} type="text" pattern="\d*"  maxLength="10" required onChange={(event) => setTel("7" + event.target.value)} />
+              <input className={styles.input} type="text" pattern="\d*" name="tel" required  maxLength="10" onChange={(event) => setTel("7" + event.target.value)} />
               </div>
-              <button onClick={(event) => submitFunction(event)} className={styles.buttonSubmit}>Отправить</button>
+              <button type="submit" onSubmit={(event) => submitFunction(event)} className={styles.buttonSubmit}>Отправить</button>
         </form>
         </div>
     </>
