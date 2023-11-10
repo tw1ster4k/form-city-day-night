9b311b166be3298ef222f23372e83a813e5f7c71
tw1ster4k <macbookpro@MacBook-Pro-Macbook.local>
2023-11-10 10:28:24 +0200

    Release2.2.0

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,8 @@ const index = () => {
   const [tel, setTel] = useState('')
   
 
-  const submitFunction = () => {
-    console.log("aaaaa")
+  const submitFunction = (event) => {
+    event.preventDefault()
     if(typeof window !== 'undefined') {
       window.Telegram.WebApp.sendData(JSON.stringify({name: name, number: tel}))
     }
@@ -20,7 +20,7 @@ const index = () => {
             <script src="https://telegram.org/js/telegram-web-app.js"></script>
         </Head>
         <div className={styles.container}>  
-        <form className={styles.form} >
+        <form onSubmit={(event) => submitFunction(event)} className={styles.form} >
           <div className={styles.itemForm} >
           <p>Имя</p>
               <input className={styles.input} style={{paddingLeft:"7px"}} pattern="^[а-яА-ЯёЁ]+$" maxLength="20" placeholder="Иван" required name="name"  onChange={(event) => setName(event.target.value)} />
@@ -30,7 +30,7 @@ const index = () => {
                 <i className={styles.numberStart} aria-hidden="true">+7</i>
               <input className={styles.input} type="text" pattern="^(([0-9]){10})$" name="tel" placeholder={"9xx-xxx-xx-xx"} required  maxLength="10" onChange={(event) => setTel("7" + event.target.value)} />
               </div>
-              <button type="submit" onClick={() => submitFunction()} className={styles.buttonSubmit}>Отправить</button>
+              <button type="submit" className={styles.buttonSubmit}>Отправить</button>
         </form>
         </div>
     </>
